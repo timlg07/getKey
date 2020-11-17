9bbd2719a6b7aeef5578dc3d8cf1ee89b5f53036
Tim Greller <timlg07@gmail.com>
2020-11-17 10:35:25 +0100

    Created the getKey c program.

---
+++ getKey.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+int main(int argc, char *argv[]) {
+  char c;
+  if (argc > 1) {
+    c = atoi(argv[1]);
+    if (!c) {
+      c = argv[1][0];
+    }
+  } else {
+    c = getch();
+  }
+
+  putchar(c);
+  return c;
+}
+
